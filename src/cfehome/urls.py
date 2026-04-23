a20430216e608d05dd3ffb96abc323bab953f547
Sanatbek Bokijonov <bokijonovs@gmail.com>
2026-04-23 16:38:02 +0500

    upd

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for main project.
+URL configuration for cfehome project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,14 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path
-from .views import homepage
+
+from .views import home_view, about_view
 
 urlpatterns = [
+    path("", home_view), #index page -> root page
+    path("about/", about_view),
+    path("hello-world/", home_view),
+    path("hello-world.html", home_view),
     path("admin/", admin.site.urls),
-    path("home/", homepage),
-    path("about/", homepage),
-    path("", homepage)
 ]
