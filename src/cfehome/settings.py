d17a1604cff243471af1148ff5fe5b42c7ad00b4
Sanatbek Bokijonov <bokijonovs@gmail.com>
2026-04-27 23:25:54 +0500

    upd1

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # my-apps
+    "commando",
     "visits",
 ]
 
@@ -97,7 +98,7 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = config("DATABASE_URL", cast=str)
+DATABASE_URL = config("DATABASE_URL", default=None )
 CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
 if DATABASE_URL is not None:
     import dj_database_url
@@ -142,8 +143,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = "static/"
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+STATIC_ROOT = BASE_DIR / "local-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
