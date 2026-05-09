b9cebd88a3fef81c2615b78fcaa4e8a0c7bbd98a
Sanatbek Bokijonov <bokijonovs@gmail.com>
2026-05-09 16:19:25 +0500

    upd

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from auth import views as auth_views
 
 from .views import home_view, about_view
 
 urlpatterns = [
     path("", home_view), #index page -> root page
     path("about/", about_view),
+    path("login/", auth_views.loginview),
+    path("login/", auth_views.registerview),
     path("hello-world/", home_view),
-    path("hello-world.html", home_view),
+    # path("hello-world.html", home_view),
     path("admin/", admin.site.urls),
 ]
